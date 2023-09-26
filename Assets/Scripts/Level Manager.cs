f2d09c2b1ffdc4b6767a87fbee4f7a854002b708
Emiliano Salim <emusalim@gmail.com>
2023-09-26 19:24:36 -0300

    LevelManager

---
+++ Assets/Scripts/Level Manager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void StartLevel(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
